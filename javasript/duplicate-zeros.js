7bb9360b6257a1901a88cdc127e433eed798dd8b
DESKTOP-G95DEUK\LENOVO <ahmedselim806.as@gmail.com>
2021-04-15 02:52:28 +0200

    start practicing js

---
+++ javasript/duplicate-zeros.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+var duplicateZeros = function(arr) {
+    
+   
+    for(let i=0;i<arr.length;++i){
+        if(!arr[i]){
+            arr.splice(i,0,0);
+           ++i;
+            arr.pop();
+           
+        }
+    }
+    
+    
+    
+    
+    
+
+    
+};
